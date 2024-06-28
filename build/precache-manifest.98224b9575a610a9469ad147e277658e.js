283a8364ca46fa905eec656a65b00f11002190d8
Harsh Vaidh <78153996+HVAIDH5@users.noreply.github.com>
2024-06-28 18:31:44 +0530

    3rd fix

---
+++ build/precache-manifest.98224b9575a610a9469ad147e277658e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3384a086b1ca2dcef245ccaf14ce5f88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96f7b7ec2981566728ec",
+    "url": "/static/css/main.db47111f.chunk.css"
+  },
+  {
+    "revision": "1df8db162fae4ead190c",
+    "url": "/static/js/2.3822c282.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.3822c282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f7b7ec2981566728ec",
+    "url": "/static/js/main.abe3e486.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  }
+]);
